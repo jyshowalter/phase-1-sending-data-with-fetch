cfb5f343d95036504146a85dd8ad85b2fa0fe298
Jason <jyshowalter@gmail.com>
2023-06-10 23:08:12 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+ // Add your code here
+function submitData(name, email){
+    fetch ('http://localhost:3000/users'), {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            }, 
+        body: JSON.stringify({
+            "name": name,
+            "email": email,  
+        })
+        }
+        .then(res => res.json())
+        .then(body => newData(body.id))
+        .catch(error => document.body.innterHTML = error.message)
+    }
+
+    function newData(newUser) {
+        document.getElementsByTagName("body")[0].innerHTML = newUser
+    }
+
+    submitData()
